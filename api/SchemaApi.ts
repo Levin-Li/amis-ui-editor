647a6c923445bce9675a8fc488b1a2dae01daefb
llw <vince2li@gmail.com>
2022-05-31 09:41:30 +0800

    init

--- api/SchemaApi.ts
+++ api/SchemaApi.ts
@@ -2,14 +2,65 @@ import axios from "axios";
 import {alert} from 'amis';
 
 import {IMainStore} from '../store';
+
+import jwtDecode from "jwt-decode";
+
 //文档地址：https://kjur.github.io/jsrsasign/api/symbols/KJUR.jws.JWS.html#.verifyJWT
 // import 'jsrsasign/lib/jsrsasign-jwths-min.js';
+
 // require('jsrsasign/lib/jsrsasign-all-min.js');
 
-const searchParams = new URL(window.location.href).searchParams;
+const searchParams = new URL(location.href).searchParams;
 
 // console.log(process.env)
 
+const isLocalhost = location.hostname === '127.0.0.1';
+
+const tokenKey = "token_" + location.href;
+const secretKey = tokenKey + "_secret";
+
+let token = searchParams.get("t");
+let secret = searchParams.get("p");
+
+if (token) {
+    localStorage.removeItem(tokenKey)
+}
+if (secret) {
+    localStorage.removeItem(secretKey)
+}
+
+if (isLocalhost && !token) {
+
+    let testToken = {
+        loadUrl: "/public/Role.json",
+        saveUrl: "/public/Save",
+        baseUrl: "127.0.0.1:" + (location.port || '80')
+    }
+
+    let testSecret = "-secret:" + new Date().getTime();
+
+    //方法描述：KJUR.jws.JWS.sign(alg, spHead, spPayload, key, pass)
+
+    // sign HS256 signature with password "aaa" implicitly handled as string
+//     sJWS = KJUR.jws.JWS.sign(null, {alg: "HS256", cty: "JWT"}, {age: 21}, "aaa");
+// // sign HS256 signature with password "6161" implicitly handled as hex
+//     sJWS = KJUR.jws.JWS.sign(null, {alg: "HS256", cty: "JWT"}, {age: 21}, "6161");
+// // sign HS256 signature with base64 password
+//     sJWS = KJUR.jws.JWS.sign(null, {alg: "HS256"}, {age: 21}, {b64: "Mi/8..a="});
+// // sign RS256 signature with PKCS#8 PEM RSA private key
+//     sJWS = KJUR.jws.JWS.sign(null, {alg: "RS256"}, {age: 21}, "-----BEGIN PRIVATE KEY...");
+// // sign RS256 signature with PKCS#8 PEM ECC private key with passcode
+//     sJWS = KJUR.jws.JWS.sign(null, {alg: "ES256"}, {age: 21},
+//         "-----BEGIN PRIVATE KEY...", "keypass");
+// // header and payload can be passed by both string and object
+//     sJWS = KJUR.jws.JWS.sign(null, '{alg:"HS256",cty:"JWT"}', '{age:21}', "aaa");
+
+    testToken = KJUR.jws.JWS.sign(null, {alg: "HS256", cty: "JWT"}, testToken, "llw@oak" + testSecret);
+
+    localStorage.setItem(tokenKey, testToken);
+
+    localStorage.setItem(secretKey, testSecret);
+}
 
 // jwt 解密密码
 //获取字符串对象
@@ -19,24 +70,40 @@ const searchParams = new URL(window.location.href).searchParams;
 // saveUrl:
 // headers:
 // }
+
 //通过token或是参数
-const token = window.localStorage.getItem("token_" + window.location.href) || searchParams.get("t")
 
-const secret = "llw@oak" + (window.localStorage.getItem("token_secret_" + window.location.href) || searchParams.get("p"))
+token = token || localStorage.getItem(tokenKey)
+//立刻删除
+localStorage.removeItem(tokenKey)
 
+//参数优先
+secret = "llw@oak" + (secret || localStorage.getItem(secretKey))
+
+//立刻删除
+localStorage.removeItem(secretKey)
+
+//参数优先
 const alg = searchParams.get("a") || 'HS256';
 
+if (isLocalhost) {
+    console.log(" token:" + token)
+    console.log(" secret:" + secret)
+    console.log(" alg:" + alg)
+}
+
 //jwt token验证失败
-// if (!KJUR.jws.JWS.verifyJWT(token, secret, {alg: [alg]})) {
-//     throw new Error("token verify fail")
-// }
+if (!KJUR.jws.JWS.verifyJWT(token, secret, {alg: [alg, 'RS256', 'ES256', 'PS256']})) {
+    throw new Error("token verify fail")
+}
 
 //token 解码，不验证
-const tokenData: any = {loadUrl: "/public/Role.json"};// jwt_decode(token, {header: false})
+// const tokenData: any = {loadUrl: "/public/Role.json"};// jwt_decode(token, {header: false})
+const tokenData: any = jwtDecode(token, {header: false})
 
 function completeUrl(url: string) {
     return (url.trim().startsWith("http://") || url.trim().startsWith("https://"))
-        ? url : ((tokenData?.baseUrl || (location.protocol + "//" + location.host)) + url);
+        ? url : ((tokenData?.baseUrl || (location.protocol + "//" + location.host)) + "/" + url);
 }
 
 function getHeaders() {
